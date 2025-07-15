a0d7c0d2a6e101f0f250c79dbf3a3bdb88d812bd
Lin Jiang <jianglin@lnjng.com>
2025-07-14 19:05:03 -0700

    feat: set up window creation and event loop with sdl

--- src/main.c
+++ src/main.c
@@ -1,5 +1,70 @@
+#include <stdbool.h>
 #include <stdio.h>
 
+#include <SDL3/SDL_error.h>
+#include <SDL3/SDL_events.h>
+#include <SDL3/SDL_hints.h>
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_render.h>
+#include <SDL3/SDL_video.h>
+
+#define INIT_WIDTH 1080
+#define INIT_HEIGHT 720
+#define pse()                                                                  \
+  printf("Error: %s", SDL_GetError());                                         \
+  return 1;
+
 int main(void) {
-  printf("Hello, World");
+  // initialize SDL with video subsystem
+  if (!SDL_Init(SDL_INIT_VIDEO)) {
+    pse();
+  }
+
+  // create a resizable window
+  SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0");
+  SDL_Window *window = SDL_CreateWindow("ped", INIT_WIDTH, INIT_HEIGHT, SDL_WINDOW_RESIZABLE);
+  if (window == NULL) {
+    pse();
+  }
+
+  // create a renderer
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+  printf("%s", SDL_GetError());
+  if (renderer == NULL) {
+    pse();
+  }
+  
+  // event loop with quit event state
+  bool quit = false;
+  while (!quit) {    
+    // handle events by repeatedly polling from event queue
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+      case SDL_EVENT_QUIT:
+        quit = true;
+        break;
+      }
+    }
+
+    // set drawing color to white
+    if (!SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255)) {
+      pse();
+    }
+
+    // clear window background with drawing color
+    if (!SDL_RenderClear(renderer)) {
+      pse();
+    }
+
+    // present the screen
+    if (!SDL_RenderPresent(renderer)) {
+      pse();
+    }
+  }
+
+  // cleanup
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 }
