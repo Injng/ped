18523d44b3b5d2d06fba158638a28c1eb175129c
Lin Jiang <jianglin@lnjng.com>
2025-07-18 00:13:08 -0700

    refactor: move glyph structs and functions to glyph.c

---
+++ src/glyph.h
@@ -0,0 +1,90 @@
+#ifndef GLYPH_H
+#define GLYPH_H
+
+#include <SDL3/SDL_surface.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
+/**
+ * struct Encoding - Defines a key-value pair with the key as the unicode
+ * codepoint and the value as the glyph texture.
+ *
+ * @key: The unicode codepoint corresponding to the character.
+ * @value: The corresponding glyph texture.
+ *
+ * The encoding struct is meant to store information linking a unicode
+ * codepoint to its respective glyph texture. It is used as a key-value pair
+ * in the stb hash map implementation, in order to quickly retrieve the
+ * corresponding texture to be rendered once the codepoint is known.
+ */
+typedef struct Encoding {
+  uint32_t key;
+  SDL_Texture *value;
+} Encoding;
+
+/**
+ * struct Glyphs - Defines a struct that contains all the information for
+ * a font's glyphs and their codepoints.
+ *
+ * @glyphs: A hash map with the codepoint as the key and the texture as the
+ * value.
+ * @font: The TTF_Font struct used to generate the glyphs.
+ * @width: The width of each glyph.
+ * @height: The height of the font.
+ *
+ * This struct stores athe hash map for codepoint-based glyph texture lookup
+ * as well as the corresponding information, such as the font used for the
+ * glyphs as well as their dimensions. The dimensions assume that the font
+ * is monospaced, or fixed-width. Non-monospace fonts should not attempt
+ * to use functions related to this struct.
+ */
+typedef struct Glyphs {
+  Encoding *glyphs;
+  TTF_Font *font;
+  int width;
+  int height;
+} Glyphs;
+
+/**
+ * init_glyphs() - Initializes a Glyphs struct using the given parameters.
+ *
+ * @font: The font used to generate the glyphs.
+ * @renderer: The renderer used to generate the textures.
+ * @color: The color of the glyphs.
+ *
+ * This function returns a pointer to a Glyphs struct with its hash map
+ * populated with codepoints and their corresponding glyph textures. The
+ * textures are set according to the passed in color and font. free_glyphs()
+ * must be called before the program exits. This function returns NULL if
+ * it fails. For error information, use SDL_GetError().
+ */
+Glyphs *init_glyphs(TTF_Font *font, SDL_Renderer *renderer, SDL_Color color);
+
+/**
+ * free_glyphs() - Frees a Glyphs struct from memory.
+ *
+ * @text: The Glyphs struct to be freed.
+ *
+ * This function frees a Glyphs struct by first iterating through the hash
+ * map and freeing all of the textures, and then freeing the memory
+ * allocated for the struct itself.
+ */
+void free_glyphs(Glyphs *text);
+
+/**
+ * render_text() - Uses a populated Glyphs struct to render text.
+ *
+ * @glyphs: The Glyphs struct to be used.
+ * @renderer: The renderer used to render the text on.
+ * @text: The text to be rendered as a dynamic array of unicode codepoints.
+ *
+ * This function utilizes the textures within the Glyphs struct to render
+ * text on the screen using the SDL_Renderer. It iterates through the given
+ * dynamic array text, passing each codepoint to the hash map. If the
+ * codepoint does not have a corresponding texture, the function will
+ * automatically skip it, which will show up as a blank space. This
+ * function returns true if successful, and false if there are errors. Use
+ * SDL_GetError() for more information.
+ */
+bool render_text(Glyphs *glyphs, SDL_Renderer *renderer, uint32_t *text);
+
+#endif // GLYPH_H
--- src/main.c
+++ src/main.c
@@ -1,43 +1,16 @@
-#include <SDL3/SDL_keycode.h>
-#include <SDL3/SDL_rect.h>
-#include <stdbool.h>
-#include <stdint.h>
 #include <stdio.h>
-
-#include <SDL3/SDL_error.h>
-#include <SDL3/SDL_events.h>
-#include <SDL3/SDL_hints.h>
-#include <SDL3/SDL_init.h>
-#include <SDL3/SDL_pixels.h>
-#include <SDL3/SDL_render.h>
-#include <SDL3/SDL_surface.h>
-#include <SDL3/SDL_video.h>
-#include <SDL3_ttf/SDL_ttf.h>
-#include <stdlib.h>
 
 #define STB_DS_IMPLEMENTATION
 #include "stb_ds.h"
+#include "glyph.h"
 
 #define INIT_WIDTH 1080
 #define INIT_HEIGHT 720
-#define GLYPHS_SIZE 123
 #define FONT_FILE "/usr/share/fonts/TTF/JetBrainsMonoNerdFontMono-Regular.ttf"
 #define pse()                                                                  \
   printf("Error: %s", SDL_GetError());                                         \
   code = 1;                                                                    \
   goto cleanup;
-
-typedef struct Encoding {
-  uint32_t key;
-  SDL_Texture *value;
-} Encoding;
-
-typedef struct Glyphs {
-  Encoding *glyphs;
-  TTF_Font *font;
-  int width;
-  int height;
-} Glyphs;
 
 TTF_Font *font = NULL;
 SDL_Window *window = NULL;
@@ -45,83 +18,6 @@ SDL_Renderer *renderer = NULL;
 TTF_TextEngine *text_engine = NULL;
 TTF_Text *text = NULL;
 Glyphs *glyphs = NULL;
-
-Glyphs *init_glyphs(TTF_Font *font, SDL_Renderer *renderer, SDL_Color color) {
-  // initialize hash map of glyphs and allocate memory for our glyphs struct
-  Encoding *textures = NULL;
-  Glyphs *glyphs = malloc(sizeof(Glyphs));
-  if (glyphs == NULL) {
-    SDL_SetError("Failed to allocate memory for Glyphs struct");
-    return NULL;
-  }
-
-  // check if font is fixed width
-  if (!TTF_FontIsFixedWidth(font)) {
-    SDL_SetError("Font must be fixed width");
-    return NULL;
-  }
-
-  // iterate through the unicode codepoints and generate textures for the font
-  for (int i = 48; i < GLYPHS_SIZE; i++) {
-    // render a blended glyph surface
-    SDL_Surface *gs = TTF_RenderGlyph_Blended(font, i, color);
-    if (gs == NULL) return NULL;
-
-    // create a texture from the surface
-    SDL_Texture *glyph = SDL_CreateTextureFromSurface(renderer, gs);
-    if (glyph  == NULL) return NULL;
-
-    // add to the hash map with the unicode codepoint as the key
-    hmput(textures, i, glyph);
-  }
-
-  // set properties and return pointer to glyphs
-  glyphs->glyphs = textures;
-  glyphs->font = font;
-  glyphs->width = textures[0].value->w;
-  glyphs->height = TTF_GetFontHeight(font);
-
-  return glyphs;
-}
-
-void free_glyphs(Glyphs *text) {
-  for (int i = 0; i < GLYPHS_SIZE; i++) {
-    SDL_DestroyTexture(text->glyphs[i].value);
-  }
-  free(text);
-}
-
-bool render_text(Glyphs *glyphs, SDL_Renderer *renderer, uint32_t *text) {
-  // exit early if no text to render
-  if (arrlen(text) == 0) return true;
-
-  // check if font is fixed width
-  if (!TTF_FontIsFixedWidth(glyphs->font)) {
-    SDL_SetError("Font must be fixed width");
-    return false;
-  }
-
-  // render each character in the dynamic array
-  for (int i = 0; i < arrlen(text); i++) {
-    // destination rectangle always starts from offest and is calculated from the start
-    SDL_FRect dst = {
-      .x = 100 + glyphs->width * i,
-      .y = 100,
-      .w = glyphs->width,
-      .h = glyphs->height
-    };
-
-    // check if text is in glyphs
-    if (hmgeti(glyphs->glyphs, text[i]) == -1) continue;
-
-    // render the texture to the destination rectangle
-    if (!SDL_RenderTexture(renderer, hmget(glyphs->glyphs, text[i]), NULL, &dst)) {
-      return false;
-    }
-  }
-
-  return true;
-}
 
 int main(void) {
   // code to return from the program with
