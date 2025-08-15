3d24bc5d44e15d5f5504f6caf7daa132ce7779b0
Lin Jiang <jianglin@lnjng.com>
2025-08-15 13:38:49 -0700

    feat: handle modifier keys

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 #include <SDL3/SDL_init.h>
+#include <SDL3/SDL_keyboard.h>
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_video.h>
 #include <SDL3_ttf/SDL_ttf.h>
@@ -88,20 +89,22 @@ int main(void)
   while (!quit) {    
     // handle events by repeatedly polling from event queue
     SDL_Event event;
+    SDL_Keycode key;
     while (SDL_PollEvent(&event)) {
       switch (event.type) {
       case SDL_EVENT_QUIT:
         quit = true;
         break;
       case SDL_EVENT_KEY_DOWN:
+        key = SDL_GetKeyFromScancode(event.key.scancode, event.key.mod, false);
         if (event.key.key == SDLK_RETURN) {
           buffer_newline(buffer, &cursor);
         } else if (event.key.key == SDLK_BACKSPACE && cursor.idx > -1) {
           if (!buffer_delete(buffer, &cursor)) {
             pse();
           }
-        } else if (validate_glyphs(event.key.key)) {
-          if (!buffer_insert(buffer, &cursor, event.key.key)) {
+        } else if (validate_glyphs(key)) {
+          if (!buffer_insert(buffer, &cursor, key)) {
             pse();
           }
         } else {
